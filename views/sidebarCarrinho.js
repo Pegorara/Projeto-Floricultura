e2f8a4764741d2a74e7ed7363747aec828059ce2
AlipioGSF <alipiotec@gmail.com>
2023-06-20 11:24:53 -0300

    Correção do contador de produto do carrinho

--- views/sidebarCarrinho.js
+++ views/sidebarCarrinho.js
@@ -16,6 +16,7 @@ function renderCardCat(flor){
   const divContentCard = document.createElement("div");
   divContentCard.classList.add("contentCart");
   const titlePrice = document.createElement("div");
+  titlePrice.classList.add("titlePrice");
   const title = document.createElement("h4");
   title.innerHTML = flor.title;
   const price = document.createElement("h4");
@@ -23,6 +24,7 @@ function renderCardCat(flor){
   titlePrice.appendChild(title);
   titlePrice.appendChild(price);
   const qtdLocal = document.createElement("div");
+  qtdLocal.classList.add("qtdLocal");
   const btn1 = document.createElement('button')
   btn1.innerHTML = "-";
   btn1.addEventListener("click", ()=>{removeItem(flor);inputNum.value = flor.qntd});
@@ -30,7 +32,7 @@ function renderCardCat(flor){
   const inputNum = document.createElement("input");
   inputNum.setAttribute("type", "number");
   inputNum.setAttribute("value", flor.qntd);
-  inputNum.addEventListener('change',({target})=>{addItem(flor,target.value); calcTotal()})
+  inputNum.addEventListener('change',({target})=>{if(target.value<0){target.value=0}else{addItem(flor,target.value)}; calcTotal()})
   qtdLocal.appendChild(inputNum);
   const btn2 = document.createElement('button');
   btn2.innerHTML = "+";
@@ -63,7 +65,7 @@ function reloadCart(){
 
 function addItem(flor, qtd) {
   if (qtd) {
-    flores.map((f) => (f.num == flor.num ? (flor.qntd = qtd) : null));
+    flores.map((f) => (f.num == flor.num ? (flor.qntd = Number(qtd)) : null));
   } else {
     flores.map((f) => (f.num == flor.num ? (flor.qntd += 1) : null));
   }
